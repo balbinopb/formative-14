aa0eded3b95be32d1b03fc1e06a4f8182888fa30
Balbino Pedro Baptista <dinubatista4@gmail.com>
2025-08-22 21:56:48 +0700

    update .env

--- main.go
+++ main.go
@@ -140,19 +140,13 @@ func DeleteBioskop(c *gin.Context) {
 
 func main() {
 
-	if err := godotenv.Load(); err != nil {
-		log.Println(".env file not found")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		_ = godotenv.Load() 
 	}
-
-	host := os.Getenv("PGHOST")
-	port := os.Getenv("PGPORT")
-	user := os.Getenv("PGUSER")
-	password := os.Getenv("PGPASSWORD")
-	dbname := os.Getenv("PGDATABASE")
 
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
+		os.Getenv("PGHOST"), os.Getenv("PGPORT"), os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"),
 	)
 
 	var err error
